a9a35805e815da4cace963cbd87388b07ea5ca91
Maxime Pigeon <pigeon.maxime1989@gmail.com>
2018-09-27 23:45:00 -0400

    first commit

---
+++ blogPostDAO.py
@@ -0,0 +1,99 @@
+
+import sys
+import re
+import datetime
+
+
+# The Blog Post Data Access Object handles interactions with the Posts collection
+class BlogPostDAO:
+    compteur = 0
+
+    # constructor for the class
+    def __init__(self, database):
+        self.db = database
+        self.posts = database.posts
+
+    # inserts the blog entry and returns a permalink for the entry
+    def insert_entry(self, title, post, tags_array, author):
+        print("inserting blog entry", title, post)
+
+        # fix up the permalink to not include whitespace
+        number_of_post = self.posts.find({})
+        exp = re.compile('\W') # match anything not alphanumeric
+        whitespace = re.compile('\s')
+        temp_title = whitespace.sub("_",title)
+        permalink = exp.sub('', temp_title)
+
+        # Build a new post
+        post = {
+                "_id":number_of_post.count(),
+                "title": title,
+                "author": author,
+                "body": post,
+                "permalink":permalink + "_" + str(number_of_post.count()),
+                "tags": tags_array,
+                "comments": [],
+                "date": datetime.datetime.utcnow()}
+
+        # now insert the post
+        # XXX TP1 Work Here to insert the post
+        self.posts.insert_one(post)
+        return permalink
+
+
+    # returns an array of num_posts posts, reverse ordered by date.
+    # XXX TP1 work here change function signature to implement pagination.
+    def get_posts(self, num_posts):
+
+        cursor = iter(())  # Using an empty itable for a placeholder so blog compiles before you make your changes
+
+        # XXX TP1 Work here to get the num_posts posts
+        cursor = self.posts.find()[:num_posts]
+        l = []
+
+        for post in cursor:
+            post['date'] = post['date'].strftime("%A, %B %d %Y at %I:%M%p") # fix up date
+            if 'tags' not in post:
+                post['tags'] = [] # fill it in if its not there already
+            if 'comments' not in post:
+                post['comments'] = []
+
+            l.append({'title':post['title'], 'body':post['body'], 'post_date':post['date'],
+                      'permalink':post['permalink'],
+                      'tags':post['tags'],
+                      'author':post['author'],
+                      'comments':post['comments']})
+
+        return l
+
+    # find a post corresponding to a particular permalink
+    def get_post_by_permalink(self, permalink):
+
+        post = None
+        # XXX TP1 Work here to retrieve the specified post
+        post = self.posts.find({'permalink': permalink})[0]
+        print("PRINTING TEST", post) #TO DO#
+        if post is not None:
+            # fix up date
+            post['date'] = post['date'].strftime("%A, %B %d %Y at %I:%M%p")
+
+        return post
+
+    # add a comment to a particular blog post
+    def add_comment(self, permalink, name, email, body):
+
+        comment = {'author': name, 'body': body}
+
+        if (email != ""):
+            comment['email'] = email
+
+        try:
+            # XXX TP1 Work here to add the comment to the designated post. When done, modify the line below to return the number of documents updated by your modification, rather than just -1.
+            number_of_document = self.posts.update_one({'permalink':permalink}, {'$push': {'comments':comment} })
+
+            return number_of_document          # return the number of documents updated
+
+        except:
+            print("Could not update the collection, error")
+            print("Unexpected error:", sys.exc_info()[0])
+            return 0
---
+++ tp1/Scripts/activate.bat
@@ -0,0 +1,30 @@
+@echo off
+set "VIRTUAL_ENV=C:\Users\Max\GOOGLE~1\AUTOMN~1\tp1\START_~1\tp1"
+
+if defined _OLD_VIRTUAL_PROMPT (
+    set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
+) else (
+    if not defined PROMPT (
+        set "PROMPT=$P$G"
+    )
+    set "_OLD_VIRTUAL_PROMPT=%PROMPT%"
+)
+set "PROMPT=(tp1) %PROMPT%"
+
+REM Don't use () to avoid problems with them in %PATH%
+if defined _OLD_VIRTUAL_PYTHONHOME goto ENDIFVHOME
+    set "_OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%"
+:ENDIFVHOME
+
+set PYTHONHOME=
+
+REM if defined _OLD_VIRTUAL_PATH (
+if not defined _OLD_VIRTUAL_PATH goto ENDIFVPATH1
+    set "PATH=%_OLD_VIRTUAL_PATH%"
+:ENDIFVPATH1
+REM ) else (
+if defined _OLD_VIRTUAL_PATH goto ENDIFVPATH2
+    set "_OLD_VIRTUAL_PATH=%PATH%"
+:ENDIFVPATH2
+
+set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
